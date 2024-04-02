858bd87f88fdbb7a2dfd3e458ee807fead2c6455
KelvinBarros <kelvinbarros92@gmail.com>
2024-04-02 19:02:55 -0300

    novo app

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const key = "dbccb0e9b201f89435154043f66988c7"
+
+function buscarCidade(cidade){
+
+}
+
+function pesquisar(){
+    const cidade = document.querySelector(".local").value
+    
+    buscarCidade(cidade)
+}
+
